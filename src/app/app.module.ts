e2f90f2394d7a633b6545d4cfc10a3d7ae2de4c3
gabriel <xaviergabriel92@gmail.com>
2018-11-28 16:10:01 -0200

    Enviando alterações

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,21 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
+import { NgxQRCodeModule } from 'ngx-qrcode2';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCEpqL9iZucfAqi-RGvYG-CvawiK4-8b98",
+  authDomain: "qr-code-d8195.firebaseapp.com",
+  databaseURL: "https://qr-code-d8195.firebaseio.com",
+  projectId: "qr-code-d8195",
+  storageBucket: "qr-code-d8195.appspot.com",
+  messagingSenderId: "283770966586"
+};
+
 @NgModule({
   declarations: [
     MyApp,
@@ -14,7 +29,10 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    NgxQRCodeModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -22,6 +40,7 @@ import { HomePage } from '../pages/home/home';
     HomePage
   ],
   providers: [
+    BarcodeScanner,
     StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,87 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, LoadingController } from 'ionic-angular';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { Observable } from 'rxjs/Observable';
+import { AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  qrData = null;
+  createdCode = null;
+  scannedCode = null;
+ 
+  private codeRef;
+  codes: Observable<any[]>;
+  codesArray: any = [];
 
-  constructor(public navCtrl: NavController) {
+  constructor(private barcodeScanner: BarcodeScanner,
+              public db: AngularFireDatabase,
+              public alertCtrl: AlertController,
+              public loadingCtrl: LoadingController) {
+
+    this.codeRef = db.list('code');
+    this.codes = db.list('code').snapshotChanges();
+    this.updateCode();
 
   }
+ 
+  updateCode(){
 
+   // this.codesArray = []
+
+    this.codes.subscribe((value) => {
+      value.forEach(data => {        
+        this.codesArray.push(data.payload.val());
+      })
+
+    })
+  }
+
+  scanCode() {
+    let loading = this.loadingCtrl.create({
+      content: 'Please wait...'
+    });
+    this.barcodeScanner.scan().then(barcodeData => {
+      loading.present();
+      if(this.codesArray.indexOf(barcodeData.text) === -1){
+
+        this.db.list('code').push(barcodeData.text)
+          .then(() => {
+            this.updateCode();
+            loading.dismiss();
+            const alert = this.alertCtrl.create({
+              title: 'ENTRADA LIBERADA',
+              subTitle: 'O ingresso esta VALIDO e a entrada do convidado esta liberada',
+              buttons: ['OK']
+            });
+            alert.present();            
+          });
+
+      } else if (this.codesArray.indexOf(barcodeData.text) > -1){
+        loading.dismiss();
+        const alert = this.alertCtrl.create({
+          title: 'NÃO LIBERAR ENTRADA',
+          subTitle: 'O ingresso esta INVALIDO favor encaminhar o convidado para a proxima saida.',
+          buttons: ['OK']
+        });
+        alert.present();
+      }
+      
+      this.scannedCode = barcodeData.text;
+    }, (err) => {
+      loading.dismiss();
+      const alert = this.alertCtrl.create({
+        title: 'ERRO',
+        subTitle: 'Reinicie o App',
+        buttons: ['OK']
+      });
+      alert.present();
+
+    });
+  }
+ 
 }
